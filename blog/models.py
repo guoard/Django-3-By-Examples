25bd4043ccedd44f7d99e6745e50edc23c40dfff
Ali <afsharzadeh1998@gmail.com>
2021-07-06 11:55:24 +0430

    try to fix bug

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -19,7 +19,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
