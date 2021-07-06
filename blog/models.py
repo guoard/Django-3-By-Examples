018a64f9aaea8d22ac19ec0b93b0b73b654a4057
Ali <afsharzadeh1998@gmail.com>
2021-07-06 09:03:46 +0430

    add url pattern for blog app and define get_absolute_url method in post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -33,3 +34,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
