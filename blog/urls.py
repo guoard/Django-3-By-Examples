635f2ce0d7ccdcbdb46ec0174b7f9524bb946431
Ali <afsharzadeh1998@gmail.com>
2021-07-07 09:50:59 +0430

    create endpoint for sharing post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
